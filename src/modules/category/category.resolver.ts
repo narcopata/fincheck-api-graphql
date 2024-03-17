e63deb8f630f87555e9990b99dc89289187943ff
Rodrigo Walace Silva <rodrigo.walace.silva@gmail.com>
2024-03-17 20:18:16 -0300

    feat: creating category graphql resources

--- src/modules/category/category.resolver.ts
+++ src/modules/category/category.resolver.ts
@@ -1,7 +1,21 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
 import { CategoryService } from './category.service';
+import { ActiveUserId } from '~shared/decorators/ActiveUserId';
+import { Category } from './entities/category.entities';
 
 @Resolver()
 export class CategoryResolver {
   constructor(private readonly categoryService: CategoryService) {}
+
+  @Query(() => [Category], {
+    name: 'categories',
+  })
+  async findAllByUserId(
+    @ActiveUserId()
+    userId: string,
+  ) {
+    const categories = await this.categoryService.findAllByUserId(userId);
+
+    return categories;
+  }
 }
